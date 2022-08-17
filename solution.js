3c0fbf6a6bff1d3e4900d886abf14ac0a5083874
Rami B <ramibasheer9@gmail.com>
2022-08-17 08:54:50 +0200

    apply solution

---
+++ solution.js
@@ -0,0 +1,15 @@
+//  1
+
+
+  
+let board = [
+    [1, 2, 3],
+    ['j3far', 'klb', 'mntak', '3rsa', 'mnyok'],
+    [true, false]
+];
+
+for (let i = 0; i < board.length; i++) {
+    for ( let j = 0; j < board[i].length; j++) {
+        console.log(board[i][j]);
+    }
+}
